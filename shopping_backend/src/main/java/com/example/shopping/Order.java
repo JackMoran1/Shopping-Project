0d63db9a52f4561d10a8c4bc8a72707bab1e9dec
march19th <dihya.sharif@gmail.com>
2023-07-18 14:47:24 -0500

    added user.java and order.java

--- shopping_backend/src/main/java/com/example/shopping/Order.java
+++ shopping_backend/src/main/java/com/example/shopping/Order.java
@@ -10,6 +10,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
+import java.util.List;
 
 @Document(collection = "orders")
 @Data
@@ -20,14 +21,48 @@ public class Order {
     private ObjectId id;
     private String orderId;
     private User user;
+    private List<Item> items;
     private Date orderDate;
-    private float price;
 
-    public Order(String orderId, User user, Date orderDate, float price) {
+    public Order(String orderId, User user, List<Item> items, Date orderDate) {
         this.orderId = orderId;
         this.user = user;
+        this.orderDate = new Date();
+    }
+    public float getOrderPrice() {
+        return items.stream().map(Item::getPrice).reduce(0f, Float::sum);
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
         this.orderDate = orderDate;
-        this.price = price;
     }
 }
 
