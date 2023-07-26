574f76c4bdc9a7b2c1921192ef5561abbfda4a1c
Whalien3520 <markusbao.no1@gmail.com>
2023-07-26 13:30:51 -0500

    Added toggling between pages, tables displaying information partially complete

--- shopping-front-end/src/App.js
+++ shopping-front-end/src/App.js
@@ -9,64 +9,52 @@ import SplitPane, {
 } from "./SplitPane";
 // import SplitPane from "react-split-pane"
 import TabContext from "./TabContext";
+import ItemsPage, {CodesPage, OrdersPage, UsersPage} from './PageFetch.js';
 
-const itemss = [
+const tabs = [
   {
     id: 1,
-    author: "Items",
+    category: "Items",
     description:
     "There should be some things about items here but I haven't gotten it to work yet",
+    page: ItemsPage
   },
   {
     id: 2,
-    author: "Users",
+    category: "Users",
     description:
     "There should be some things about users here but I haven't gotten it to work yet",
+    page: UsersPage
   },
   {
     id: 3,
-    author: "Discount codes",
+    category: "Discount codes",
     description:
     "There should be some things about discount codes here but I haven't gotten it to work yet",
+    page: CodesPage
   },
   {
     id: 4,
-    author: "Orders",
+    category: "Orders",
     description:
     "There should be some things about orders here but I haven't gotten it to work yet",
+    page: OrdersPage
   }
 ]
 
 function App() {
 
-  /*const [item, setItems] = useState();
-
-  const getItems = async() =>{
-    try{
-      const response = await api.get("/items/tx1111");
-      console.log(response.data)
-      setItems(response.data);
-    }
-    catch(error){
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    getItems();
-  },[])*/
-
-  const [currItemss, setCurrItemss] = useState(1);
+  const [currTab, setCurrTab] = useState(1);
 
   return (
     <div className="App">
-      <TabContext.Provider value={{ itemss, currItemss, setCurrItemss }}>
+      <TabContext.Provider value={{ tabs, currTab, setCurrTab }}>
         <SplitPane className="split-pane-row">
-          <SplitPaneLeft >
-          </SplitPaneLeft>
+          <SplitPaneLeft />
           <Divider className="separator-col" />
 
-          <SplitPaneRight />
+          <SplitPaneRight>
+          </SplitPaneRight>
         </SplitPane>
       </TabContext.Provider>
     </div>
--- shopping-front-end/src/SplitPane.js
+++ shopping-front-end/src/SplitPane.js
@@ -8,44 +8,13 @@ import React, {
 import SplitPaneContext from "./SplitPaneContext";
 import TabContext from "./TabContext";
 import api from './api/axiosConfig'
+import ItemsPage, {UsersPage} from './PageFetch.js';
 
 const SplitPane = ({ children, ...props }) => {
     const [clientHeight, setClientHeight] = useState(null);
     const [clientWidth, setClientWidth] = useState(null);
     const yDividerPos = useRef(null);
     const xDividerPos = useRef(null);
-  
-    const onMouseHoldDown = (e) => {
-      yDividerPos.current = e.clientY;
-      xDividerPos.current = e.clientX;
-    };
-  
-    const onMouseHoldUp = () => {
-      yDividerPos.current = null;
-      xDividerPos.current = null;
-    };
-  
-    const onMouseHoldMove = (e) => {
-      if (!yDividerPos.current && !xDividerPos.current) {
-        return;
-      }
-  
-      setClientHeight(clientHeight + e.clientY - yDividerPos.current);
-      setClientWidth(clientWidth + e.clientX - xDividerPos.current);
-  
-      yDividerPos.current = e.clientY;
-      xDividerPos.current = e.clientX;
-    };
-  
-    useEffect(() => {
-      document.addEventListener("mouseup", onMouseHoldUp);
-      document.addEventListener("mousemove", onMouseHoldMove);
-  
-      return () => {
-        document.removeEventListener("mouseup", onMouseHoldUp);
-        document.removeEventListener("mousemove", onMouseHoldMove);
-      };
-    });
   
     return (
       <div {...props}>
@@ -55,7 +24,6 @@ const SplitPane = ({ children, ...props }) => {
             setClientHeight,
             clientWidth,
             setClientWidth,
-            onMouseHoldDown,
           }}
         >
           {children}
@@ -67,35 +35,35 @@ const SplitPane = ({ children, ...props }) => {
 export const Divider = (props) => {
     const { onMouseHoldDown } = useContext(SplitPaneContext);
 
-    return <div {...props} onMouseDown={onMouseHoldDown} />;
+    return <div {...props} />;
 };
 
 
 export const SplitPaneLeft=(props) => {
     const leftRef = createRef();
-    const {itemss, setCurrItemss } = useContext(TabContext);
-    /*const {clientWidth, setClientWidth } = useContext(SplitPaneContext);
+    const {tabs, setCurrTab } = useContext(TabContext);
+    const {clientWidth, setClientWidth } = useContext(SplitPaneContext);
     
 
     useEffect(() => {
         if (!clientWidth) {
-            setClientWidth(topRef.current.clientWidth / 2);
+            setClientWidth(leftRef.current.clientWidth / 2);
             return;
         }
 
-        topRef.current.style.minWidth = clientWidth + "px";
-        topRef.current.style.maxWidth = clientWidth + "px";
-    }, [clientWidth]);*/
+        leftRef.current.style.minWidth = clientWidth + "px";
+        leftRef.current.style.maxWidth = clientWidth + "px";
+    }, [clientWidth]);
 
     return  (
       <div {...props} className="split-pane-left" ref={leftRef}>
         <h1>Shopping Project</h1>
           <ul>
-            {itemss.map((el, i) => {
+            {tabs.map((el, i) => {
               return (
                 <li key={i}>
-                  <a href="#" onClick={() => setCurrItemss(el.id)}>
-                    {el.author}
+                  <a href="#" onClick={() => setCurrTab(el.id)}>
+                    {el.category}
                   </a>
                 </li>
               );
@@ -105,33 +73,12 @@ export const SplitPaneLeft=(props) => {
 };
 
 export const SplitPaneRight = (props) => {
-    const { itemss, currItemss } = useContext(TabContext);
-    const itemsss = itemss.find((el) => el.id === currItemss);
-
-    const [item, setItems] = useState();
-  
-    const getItems = async() =>{
-      try{
-        const response = await api.get("/items/tx1111");
-        //console.log(response.data.itemId)
-        setItems(response.data);
-      }
-      catch(error){
-        console.log(error)
-      }
-    }
-  
-    useEffect(() => {
-      getItems();
-    },[])
+    const { tabs, currTab } = useContext(TabContext);
+    const tab = tabs.find((el) => el.id === currTab);
 
     return (
         <div {...props} className="split-pane-right">
-            <div className="quote">
-                <blockquote>{itemsss.description}</blockquote>
-                {/*<span>{item.author}</span>*/}
-                {/*<h1>{item.itemId}</h1>*/}
-            </div>
+            {tab.page()}
         </div>
     );
 };
